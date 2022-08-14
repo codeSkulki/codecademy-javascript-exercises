7feb8110fda321a9be98705087bf70d38a13e6f1
Dev <63323219+codeSkulki@users.noreply.github.com>
2022-08-14 05:52:11 -0400

    map method baseline

---
+++ Exercises/Iterators/003 The .map() Method.js
@@ -0,0 +1,10 @@
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
