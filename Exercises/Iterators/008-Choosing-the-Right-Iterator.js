263ad7ebd501f34dfe87316176fb344b95ad7602
Dev <63323219+codeSkulki@users.noreply.github.com>
2022-08-14 06:57:21 -0400

    found use cases for iterators

--- Exercises/Iterators/008-Choosing-the-Right-Iterator.js
+++ Exercises/Iterators/008-Choosing-the-Right-Iterator.js
@@ -3,20 +3,20 @@ const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', '
 const nums = [1, 50, 75, 200, 350, 525, 1000];
 
 //  Choose a method that will return undefined
-cities.method(city => console.log('Have you visited ' + city + '?'));
+cities.forEach(city => console.log('Have you visited ' + city + '?'));
 
 // Choose a method that will return a new array
-const longCities = cities.method(city => city.length > 7);
+const longCities = cities.filter(city => city.length > 7);
 
 // Choose a method that will return a single value
-const word = cities.method((acc, currVal) => {
+const word = cities.reduce((acc, currVal) => {
   return acc + currVal[0]
 }, "C");
 
 console.log(word)
 
 // Choose a method that will return a new array
-const smallerNums = nums.method(num => num - 5);
+const smallerNums = nums.map(num => num - 5);
 
 // Choose a method that will return a boolean value
-nums.method(num => num < 0);
+nums.some(num => num < 0);//or .every()!!
