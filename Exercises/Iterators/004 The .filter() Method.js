2033886b5b2d507353da45529641194837b29be6
Dev <63323219+codeSkulki@users.noreply.github.com>
2022-08-14 06:03:17 -0400

    learning the .filter() method

---
+++ Exercises/Iterators/004 The .filter() Method.js
@@ -0,0 +1,10 @@
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+
+// Call .filter() on favoriteWords below
+
