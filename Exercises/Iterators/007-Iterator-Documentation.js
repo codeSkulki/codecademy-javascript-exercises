7cae664865f7f2b853e86185467a177deab3c965
Dev <63323219+codeSkulki@users.noreply.github.com>
2022-08-14 06:41:55 -0400

    iterator doc baseline

---
+++ Exercises/Iterators/007-Iterator-Documentation.js
@@ -0,0 +1,15 @@
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+// Something is missing in the method call below
+
+console.log(words.some(() => {
+  return word.length < 6;
+}));
+
+// Use filter to create a new array
+
+
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+// console.log(interestingWords.every((word) => { } ));
