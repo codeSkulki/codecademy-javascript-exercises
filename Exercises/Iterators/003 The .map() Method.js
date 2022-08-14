ca7a03f95eedf4e6c62a0465157736b8b8494a6c
Dev <63323219+codeSkulki@users.noreply.github.com>
2022-08-14 06:02:17 -0400

    utilized the map function

--- Exercises/Iterators/003 The .map() Method.js
+++ Exercises/Iterators/003 The .map() Method.js
@@ -1,10 +1,17 @@
 const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
 // Create the secretMessage array below
-
+const secretMessage = animals.map(element => {
+  return element[0]
+});
 
 console.log(secretMessage.join(''));
 
 const bigNumbers = [100, 200, 300, 400, 500];
 
 // Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(element => {
+  return element / 100;
+})
+
+console.log(smallNumbers)
