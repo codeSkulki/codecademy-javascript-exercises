40361826df9106dc428dd63acff930590de82898
Dev <63323219+codeSkulki@users.noreply.github.com>
2022-08-15 01:18:10 -0400

    utilized callback functions

--- Exercises/Iterators/009-Iterator-Challenges.js
+++ Exercises/Iterators/009-Iterator-Challenges.js
@@ -1,5 +1,22 @@
 // Define a callback function before you use it in a iterator.
 
+        //function to callback in specificMessage
+        function greeting(name) {
+            console.log(`Hello, ${name}`);
+        }
+        //function to callback in specificMessage
+        function farewell(name) {
+            console.log(`Goodbye, ${name}`);
+        }
+        
+        //function to utilize callback functions
+        function specificMessage(callback, name) {
+            callback(name);
+        }
+        
+        //logs of greetings/farewells for custom names
+        specificMessage(greeting, 'Tom');
+        specificMessage(farewell, 'John');
 
 
 // Chain two iteration methods on the same array.
