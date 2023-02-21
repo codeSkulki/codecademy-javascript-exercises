eea10e6dfa7f2c289d1e2b69c93119d40f7bb6a7
Dev <63323219+codeSkulki@users.noreply.github.com>
2023-02-20 23:37:07 -0500

    did some object exercises

---
+++ Exercises/Objects/002-Accessing-Properties.js
@@ -0,0 +1,13 @@
+let spaceship = {
+    homePlanet: 'Earth',
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel',
+    numCrew: 5,
+    flightPath: ['Venus', 'Mars', 'Saturn']
+  };
+  
+  // Write your code below
+  
+  let crewCount = spaceship.numCrew;
+  let planetArray = spaceship.flightPath;
+  
---
+++ Exercises/Objects/004-Property-Assignment.js
@@ -0,0 +1,13 @@
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    homePlanet : 'Earth',
+    color: 'silver',
+    'Secret Mission' : 'Discover life outside of Earth.'
+  };
+  
+  // Write your code below
+  spaceship['color'] = 'glorious gold';
+  
+  spaceship['numEngines'] = 5;
+  
+  delete spaceship['Secret Mission'];
